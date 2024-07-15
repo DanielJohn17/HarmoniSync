04d3692d172f263bc548ea188fb8add04b1c1282
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-07-15 15:41:55 +0300

    bug fix

--- backend/create_database.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS harmoniSync_db;
-CREATE USER IF NOT EXISTS 'harmoniSync_dev'@'localhost' IDENTIFIED BY 'dev';
-GRANT ALL PRIVILEGES ON harmoniSync_db.* TO 'harmoniSync_dev'@'localhost';
-FLUSH PRIVILEGES;
