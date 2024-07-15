f2c24a9e63ef82a5361010a5041b3c5e6ddd369b
DanielJohn17 <danielyohannes38@gmail.com>
2024-07-15 17:52:27 +0300

    Minor fix

--- backend/api/spotify_api/requests/spotify_search.py
+++ backend/api/spotify_api/requests/spotify_search.py
@@ -3,12 +3,8 @@ from api.spotify_api.config import spotify_auth
 import requests
 import json
 
-
 class SpotifySearch:
-    # def calc_offset(offset, limit):
-
-
-    def search(self, query_user, search_types, limit=20):
+    def search(self, query_user, search_types, limit=1):
         url = "https://api.spotify.com/v1/search"
         headers = spotify_auth.get_auth_header()
 
@@ -23,29 +19,49 @@ class SpotifySearch:
         result = requests.get(url, headers=headers, params=params)
 
         if result.status_code != 200:
-            return {"error": "Faild to retrive data"}
-        
-        json_result = json.loads(result.content)
+            return {"error": "Failed to retrieve data"}
 
+        json_result = json.loads(result.content)
         search_results = {}
 
         for search_type in search_types:
             final_items = []
             items = json_result.get(search_type + 's', {}).get('items', [])
-
             for item in items:
+                if item is None:
+                    continue
+
+                # Extracting values with safety checks
+                item_id = item.get("id")
+                name = item.get("name")
+                duration = item.get("duration_ms")
+                followers = item.get("followers", {}).get("total")
+                genres = item.get("genres", [])
+                images = item.get("album", {}).get("images", [])[0].get("url") \
+                        if search_type == 'track' else item.get("images", [])[0].get("url")\
+                         if item.get("images", []) else None
+
+                spotify_link = item.get("external_urls", {}).get("spotify")
+
+                artists = item.get('artists')
+                if artists and len(artists) > 0:
+                    artist_name_url = [artists[0].get('name'), artists[0].get('external_urls', {}).get('spotify')]
+                else:
+                    artist_name_url = [None, None]
+
                 final_items.append({
-                    "id": item.get("id"),
-                    "name": item.get("name"),
-                    "followers": item.get("followers", {}).get("total"),
-                    "genres": item.get("genres", []),
+                    "id": item_id,
+                    "name": name,
+                    "duration": duration,
+                    "followers": followers,
+                    "genres": genres,
                     "type": search_type,
-                    "images": item.get("images", [])[0].get("url") if item.get("images", []) else None,
-                    "spotify_link": item.get("external_urls", {}).get("spotify"),
+                    "images": images,
+                    "spotify_link": spotify_link,
+                    "artist_name_url": artist_name_url,
                 })
             search_results[search_type + 's'] = final_items
-        
+
         return search_results
 
 spotify_search = SpotifySearch()
-
