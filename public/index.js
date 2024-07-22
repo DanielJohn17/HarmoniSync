95351a3445d550831016bd4c433fc88fabfb6962
DanielJohn17 <danielyohannes38@gmail.com>
2024-07-22 20:28:33 +0300

    loading svg

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ const BillboardIcon = "/assets/icons/billboard-icon.png";
 const DonateIcon = "/assets/icons/donate-icon.png";
 const Placeholder = "/assets/placeholder.png";
 const PlaylistIcon = "/assets/icons/playlist-icon.png";
+const Loader = "/assets/infinite-spinner.svg";
 const AlbumPlaceholder = "https://iili.io/HlHy9Yx.png";
 const PlaylistPlaceholder =
   "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQGsk72Lmip_eMTbd3Yd6uVaGPZH8ttMbeth8vrAKDNgv7N3Of8";
@@ -15,6 +16,7 @@ export {
   DonateIcon,
   Placeholder,
   PlaylistIcon,
+  Loader,
   AlbumPlaceholder,
   PlaylistPlaceholder,
   ArtistPlaceholder,
