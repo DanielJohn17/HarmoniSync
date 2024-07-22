ad3f0ffeba514fd2f2f6b840a20e478c10edea7d
DanielJohn17 <danielyohannes38@gmail.com>
2024-07-23 00:01:17 +0300

    removed console log

--- components/NotificationSlider/Notify.jsx
+++ components/NotificationSlider/Notify.jsx
@@ -2,7 +2,6 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import "./Notify.css";
 
 const Notify = ({ message, isVisible }) => {
-  console.log("Notification: " + message);
   return (
     <div className={`notification-sider ${isVisible ? "visible" : ""}`}>
       <div className="flex align-center justify-center gap-1">
